c9e41bfeb78012adaed6edf12dc3e64e6808cceb
Marcus <marcusbullock@hotmail.co.uk>
2016-01-19 14:07:35 +0000

    Phase 1 of Boris Bikes completed

---
+++ spec/docking_station_spec.rb
@@ -0,0 +1,15 @@
+# In spec/docking_station_spec.rb
+
+require "docking_station"
+
+
+describe DockingStation do
+  it {is_expected.to respond_to(:release_bike)}
+
+  it "expects station to release working bikes" do
+    bike = subject.release_bike
+    expect(bike).to be_working
+  end
+
+
+end
