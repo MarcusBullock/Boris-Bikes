e447c020113ab4f0267b48e7a7e31a607f2a2232
Marcus <marcusbullock@hotmail.co.uk>
2016-01-22 15:49:24 +0000

    Got further into chapter 21

--- lib/van.rb
+++ lib/van.rb
@@ -1,6 +1,17 @@
+require './lib/docking_station.rb'
+
+
 class Van
 
-  def dock_bikes_to_fix(station)
+  attr_accessor :to_fix
+
+  def dock_bikes_to_fix(broken_bikes)
+    @to_fix = []
+    @to_fix.push(broken_bikes)
+  end
+
+  def release_to_fix
+    
   end
 
 end
--- spec/van_spec.rb
+++ spec/van_spec.rb
@@ -5,8 +5,26 @@ describe Van do
   #let (:bike){double("bike")}
   it {is_expected.to respond_to(:dock_bikes_to_fix).with(1).argument}
 
+  # let (:bike){double("bike")}
+  # let (:bike){double("bike2")}
+  # let (:broken_bikes){double("brokenbikes")}
+
   #it 'should accept some bikes' do
-    
+  it "returns a new array of broken bikes when we call dock_bikes_to_fix" do
+    # let (:broken_bikes){double("brokenbikes")}
+    array_elements = double("array_elements")
+    array_elements = ["a","b","c"].pop
+    expect(subject.dock_bikes_to_fix(array_elements)).to eq [array_elements]
+  end
+
+  it {is_expected.to respond_to(:release_to_fix)}
+
+  it "releases the bikes" do
+    to_fix = [a,b,c]
+    expect(subject.release_to_fix).to eq (abc)
+
+  end
+
 
 
 end
